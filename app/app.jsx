50ef249b897058b89331d6b41776fd715340b478
mjdude <mo@syncspot.net>
2016-05-25 19:00:14 -0700

    added weather and about route

--- app/app.jsx
+++ app/app.jsx
@@ -2,9 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="about" component={About}></Route>
+      <IndexRoute component={Weather}></IndexRoute>
 
     </Route>
 
