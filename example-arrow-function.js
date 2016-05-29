d25742aed2938a5ab46b2255f0086d42d23c20c7
mjdude <mo@syncspot.net>
2016-05-29 11:54:57 -0700

    arrow function examples updates

--- example-arrow-function.js
+++ example-arrow-function.js
@@ -1,11 +1,43 @@
-var names = ['mo', 'jo', 'andrew'];
+// var names = ['mo', 'jo', 'andrew'];
+//
+// names.forEach(function(name){
+//   console.log('foreach ',name);
+// });
+//
+// names.forEach((names) => {
+//   console.log('arrowFunc ' , names);
+// });
+//
+// names.forEach((name) => console.log(name));
 
-names.forEach(function(name){
-  console.log('foreach ',name);
-});
+// var returnMe = (name) => name + '!';
+//
+// console.log(returnMe('Mo'));
+//
+// var person = {
+//   name: 'Mo',
+//   greet: function(){
+//     names.forEach((name) => {
+//       console.log(this.name + ' Hi to ' + name);
+//     });
+//   },
+// };
+//
+// person.greet();
 
-names.forEach((names) => {
-  console.log('arrowFunc ' , names);
-});
+// challange area
 
-names.forEach((name) => console.log(name));
+
+function add (a , b){
+  return a + b;
+};
+
+var addStatement = (a,b) => {
+  return a + b;
+};
+
+var addExpression = (a,b) => a+b;
+
+console.log( add( 1, 3), 'standard function');
+console.log(addStatement(10,0), 'arrow function');
+console.log(addExpression(11,0) , 'single line arrow function');
