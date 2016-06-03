f6d27d83bb3880a74f62505ae70adab5cd0639d0
mjdude <mo@syncspot.net>
2016-06-02 20:06:50 -0700

    updated about page with links to tools to build app

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,24 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // stateless functinoal component , no need to use React.createClass
 // because all we do is render
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the abouts page</p>
+      <h1 className="text-center">About</h1>
+      <p>Welcome to the abouts page. This app allows you to query for the weather in any city
+      in the world. Below i've put in a few links to tools used to build this app</p>
+    <ul>
+      <li>
+          <a href="https://facebook.github.io/react/">React as a front end library</a>
+      </li>
+      <li>
+          <a href="http://openweathermap.org/">Open weather maps api for access to weather data</a>
+      </li>
+      <li>
+        <a href="http://foundation.zurb.com/">Foundation to style the page</a>
+      </li>
+    </ul>
     </div>
   );
 };
