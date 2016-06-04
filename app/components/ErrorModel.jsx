bdd0f92834b82ee3bf8194b4991c0cf9413455f2
mjdude <mo@syncspot.net>
2016-06-04 13:00:44 -0700

    updated error modal

--- app/components/ErrorModel.jsx
+++ app/components/ErrorModel.jsx
@@ -1,17 +1,31 @@
 var React = require('react');
 
 var ErrorModel = React.createClass({
+  getDefaultProps: function(){
+    return {
+      title: 'Error',
+    }
+  },
+
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired,
+  },
+
   componentDidMount: function(){
     var model = new Foundation.Reveal($('#error-model'));
     model.open();
   },
   render: function(){
+
+    var {title , message} = this.props;
+
     return (
-      <div id="error-model" className="reveal tiny text-center" data-reveal>
-        <h4>Some title</h4>
-        <p>Our Error Message</p>
+      <div id="error-model" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
         <p>
-          <button className="button hollow" data-close>
+          <button className="button hollow" data-close="">
             Okay
           </button>
         </p>
